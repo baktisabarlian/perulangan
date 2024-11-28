27305067c8aac39c6f7e06225f211350b720267b
baktisabarlian <baktibsh31@gmail.com>
2024-11-28 20:37:01 +0800

    Add files via upload

---
+++ soal3.php
@@ -0,0 +1,11 @@
+<?php
+$angka = readline();
+
+$jumlahDigit = 15;
+
+for ($i = 1; $i < 6; $i++){
+    for ($j = 1; $j < 6; $j++){
+        echo "ini hitungan ($i + $j)<br>";
+    }
+}
+echo "Jumlah total digit:$jumlahDigit ";
